9366b346c4f122c4f081a0fe14087ea486420e13
linelind <line.toresby@gmail.com>
2022-04-24 11:25:28 +0200

    added texture and added html and styling to shop.

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ import { baseUrl } from "./settings/api.js";
 const productsUrl = baseUrl + "products";
 
 (async function () {
-    const prodContainer = document.querySelector(".products-container");
+    const prodContainer = document.querySelector(".shop--products-container");
 
     try {
         const response = await fetch(productsUrl);
@@ -14,10 +14,14 @@ const productsUrl = baseUrl + "products";
         prodContainer.innerHTML = "";
 
         json.forEach(function (product) {
-            prodContainer.innerHTML += `<a class="productcard" href="productspecific.html?id=${product.id}">
-                                            <img src=http://localhost:1337${product.image.url} alt="${product.image.alternativeText}" />
-                                            <h3>${product.title}</h3>
-                                            <p>${product.price}</p>
+            prodContainer.innerHTML += `<a class="productcard" href="details.html?id=${product.id}">
+                                            <div class="productcard-imagecontainer">
+                                                <img src=http://localhost:1337${product.image.url} alt="${product.image.alternativeText}" />
+                                            </div>
+                                            <div class="productcard-textcontainer">
+                                                <h3>${product.title}</h3>
+                                                <p>${product.price}</p>
+                                            </div>
                                         </a>`;
         });
     } catch (error) {
