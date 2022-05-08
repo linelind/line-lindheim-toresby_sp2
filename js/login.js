30750d9d5fb4eb00a96117ed0393a6c9236ad045
linelind <line.toresby@gmail.com>
2022-05-08 14:20:53 +0200

    built login form that saves user and redirects.

--- js/login.js
+++ js/login.js
@@ -1,8 +1,62 @@
+import { displayMessage } from "./ui/displayMessage.js";
+import { saveToken, saveUser } from "./utils/storage.js";
+import { baseUrl } from "./settings/api.js";
+
 const form = document.querySelector(".login-form");
 const username = document.querySelector("#username");
 const usernameError = document.querySelector("#usernameError");
 const password = document.querySelector("#password");
 const passwordError = document.querySelector("#passwordError");
+const formMessage = document.querySelector(".message-container");
+
+form.addEventListener("submit", submitForm);
+
+function submitForm(event) {
+    event.preventDefault();
+
+    formMessage.innerHTML = "";
+
+    const usernameValue = username.value.trim();
+    const passwordValue = password.value.trim();
+
+    if (usernameValue.length === 0 || passwordValue === 0) {
+        return displayMessage("warning", "Please fill the form", ".message-container");
+    }
+
+    doLogin(usernameValue, passwordValue);
+}
+
+async function doLogin(username, password) {
+    const url = baseUrl + "auth/local";
+
+    const data = JSON.stringify({ identifier: username, password: password });
+
+    const options = {
+        method: "POST",
+        body: data,
+        headers: {
+            "Content-Type": "application/json",
+        },
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const json = await response.json();
+
+        if (json.user) {
+            saveToken(json.jwt);
+            saveUser(json.user);
+
+            location.href = "admin.html";
+        }
+
+        if (json.error) {
+            displayMessage("warning", "Invalid login details", ".message-container");
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 function validateForm(event) {
     event.preventDefault();
---
+++ js/utils/storage.js
@@ -0,0 +1,38 @@
+const tokenKey = "token";
+const userKey = "user";
+
+export function saveToken(token) {
+    saveToStorage(tokenKey, token);
+}
+
+export function getToken() {
+    return getFromStorage(tokenKey);
+}
+
+export function saveUser(user) {
+    saveToStorage(userKey, user);
+}
+
+export function getUsername() {
+    const user = getFromStorage(userKey);
+
+    if (user) {
+        return user.username;
+    }
+
+    return null;
+}
+
+function saveToStorage(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+}
+
+function getFromStorage(key) {
+    const value = localStorage.getItem(key);
+
+    if (!value) {
+        return [];
+    }
+
+    return JSON.parse(value);
+}
